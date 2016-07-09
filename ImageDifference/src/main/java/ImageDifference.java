662748fde92c72182eefaf9c4dcbc96cae228587
ankit07 <ankit07@ankit07-nb>
2016-07-09 22:00:37 +0545

    Modular project design

---
+++ ImageDifference/src/main/java/ImageDifference.java
@@ -0,0 +1,9 @@
+/**
+ * Created by ankit07 on 7/9/16.
+ */
+public class ImageDifference {
+
+    public static void main(String[] args) {
+        System.out.println("HEREE");
+    }
+}
---
+++ PixelManipulation/src/main/java/PixelManipulation.java
@@ -0,0 +1,8 @@
+/**
+ * Created by ankit07 on 7/9/16.
+ */
+public class PixelManipulation {
+    public static void main(String[] args) {
+        System.out.println("KJEKRJEKJR");
+    }
+}
